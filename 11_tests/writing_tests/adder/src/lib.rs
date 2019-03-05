b45cd32fcb207b8e737bcfe67845ceb7f3d58818
ccdle12 <chris.coverdale24@gmail.com>
2019-03-05 17:55:25 +0000

    11: adding lib file

--- 11_tests/writing_tests/adder/src/lib.rs
+++ 11_tests/writing_tests/adder/src/lib.rs
@@ -1,7 +1,44 @@
+// This folder was generated using:
+// $ cargo new adder --lib
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("FAIL!");
+    // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
