a56f16840231fa2910827e654b1c8183d25123a1
ccdle12 <chris.coverdale24@gmail.com>
2019-03-05 14:43:28 +0000

    10: adding trait bound conditional implementation

--- 10_generics_types_traits_lifetimes/traits/src/main.rs
+++ 10_generics_types_traits_lifetimes/traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 // We cannot use external traits on external types.
 pub struct NewsArticle {
     pub headline: String,
@@ -112,4 +114,27 @@ fn main() {
     let tweet = returns_summarizable();
     println!("tweet summarized: {}", tweet.summarize());
     notify(tweet);
+}
+
+/// Using Trait Bounds to Conditionally Implemented Methods.
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    // Implements the Display function.
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x)
+        } else {
+            println!("The largest member is y = {}", self.y)
+        }
+    }
 }
