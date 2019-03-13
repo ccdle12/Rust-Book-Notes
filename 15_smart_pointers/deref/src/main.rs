69087a226165f25100d0983772d06fcb3bcd0242
ccdle12 <chris.coverdale24@gmail.com>
2019-03-13 13:43:37 +0000

    15: updating deref

--- 15_smart_pointers/deref/src/main.rs
+++ 15_smart_pointers/deref/src/main.rs
@@ -1,3 +1,27 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// Without the Deref trair, the compiler can only dereference &.
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // returns a reference to the value we want to access.
+        &self.0
+    }
+}
+
+fn hello(msg: &str) {
+    println!("Hello {}", msg)
+}
+
 fn main() {
     // We reference and borrow the i32 from x and assign to y.
     let x = 5;
@@ -14,4 +38,16 @@ fn main() {
     let y = Box::new(x);
     assert_eq!(10, x);
     assert_eq!(10, *y);
+
+    let x = 2;
+    let y = MyBox::new(x);
+    assert_eq!(2, x);
+    // Behind the scenes, rust substitutes * witht he defer function.
+    assert_eq!(2, *y);
+
+    // Deref coercion allows easier referencing, implemented via Deref trait.
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // If rust didn't implement deref coercion,m we have to right the above as...
+    // hello(&(*m)[..])
 }
