110f18dfc5d1cba658b4f63312e7a7a87e396773
ccdle12 <chris.coverdale24@gmail.com>
2019-03-25 21:58:18 +0800

    16: adding blocking in channel example

--- 16_fearless_concurrency/message_passing/src/main.rs
+++ 16_fearless_concurrency/message_passing/src/main.rs
@@ -1,3 +1,19 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
     println!("Hello, world!");
+    // mpsc = "multiple producer, single consumer".
+    // Fan in pattern - multiple producers a single consumer.
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    // recv() will block until the message  has been received on the channel.
+    // try_recv() will not block.
+    let received = rx.recv().unwrap();
+    println!("Got {}", received);
 }
