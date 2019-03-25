b940188d1e39191b16f2a15f4e50b2cdca7725b2
ccdle12 <chris.coverdale24@gmail.com>
2019-03-25 21:46:27 +0800

    16: adding example of closure borrowing

--- 16_fearless_concurrency/threads/src/main.rs
+++ 16_fearless_concurrency/threads/src/main.rs
@@ -14,6 +14,10 @@ fn main() {
         }
     });
 
+    // Move the join call, blocks the outer for loop from running until the
+    // handle is finished.
+    handle.join().unwrap();
+
     for i in 1..15 {
         println!("hi number {} from main thread", i);
         thread::sleep(Duration::from_millis(1));
@@ -23,5 +27,16 @@ fn main() {
     // In the log prints, the threads alternate, when the outer loop finishes,
     // the loop on the spawned thread continues, the program blocks until it
     // finishes.
+    // handle.join().unwrap();
+
+    // Using move to borrow a variable in a thread. Since the thread closure
+    // has no knowledge of it's outside varibales we need to use "move", to
+    // borrow the outside variable in the closure.
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
     handle.join().unwrap();
 }
