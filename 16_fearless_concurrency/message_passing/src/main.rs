61d0d294ebde32e29294b4852cf14c67e2005262
ccdle12 <chris.coverdale24@gmail.com>
2019-03-26 04:36:13 +0800

    16: adding mpsc::Sender example for fan in

--- 16_fearless_concurrency/message_passing/src/main.rs
+++ 16_fearless_concurrency/message_passing/src/main.rs
@@ -39,4 +39,40 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+
+    // Fan in example implementation, 2 threads, writing in threads to rx.
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
