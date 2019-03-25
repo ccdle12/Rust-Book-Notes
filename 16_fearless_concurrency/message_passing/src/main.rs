2f658b48885cb6afc630a5a201ad866c35b01552
ccdle12 <chris.coverdale24@gmail.com>
2019-03-26 03:56:34 +0800

    16: adding fan in pattern

--- 16_fearless_concurrency/message_passing/src/main.rs
+++ 16_fearless_concurrency/message_passing/src/main.rs
@@ -1,19 +1,42 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("Hello, world!");
     // mpsc = "multiple producer, single consumer".
     // Fan in pattern - multiple producers a single consumer.
+    // tx = transceiver
+    // rx = receiver
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        // Single val sent on the thread.
+        // let val = String::from("hi");
+        // tx.send(val).unwrap();
+
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            // The send function takes ownership of val, sending it down the
+            // channel for the receiver to take ownership.
+            // println!("val is {}", val);
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
     // recv() will block until the message  has been received on the channel.
     // try_recv() will not block.
-    let received = rx.recv().unwrap();
-    println!("Got {}", received);
+    // let received = rx.recv().unwrap();
+    // println!("Got {}", received);
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
