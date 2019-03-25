ceb9b3b6d6b259bdc97d011796341918c91509c8
ccdle12 <chris.coverdale24@gmail.com>
2019-03-25 21:38:30 +0800

    16/fearless_concurrency: adding thread spawning example

--- 16_fearless_concurrency/threads/src/main.rs
+++ 16_fearless_concurrency/threads/src/main.rs
@@ -1,3 +1,27 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Rust uses 1:1 threads.
+    // Assigning the spawned thread to handle and calling join() later on will
+    // wait for the thread to finish.
+    let handle = thread::spawn(|| {
+        for i in 1..20 {
+            println!("hi number {} from the spawned thread!", i);
+            // Sleep stops execution for some time, allowing the other thread
+            // to continue.
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..15 {
+        println!("hi number {} from main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Waits for the thread to finish by calling join().
+    // In the log prints, the threads alternate, when the outer loop finishes,
+    // the loop on the spawned thread continues, the program blocks until it
+    // finishes.
+    handle.join().unwrap();
 }
