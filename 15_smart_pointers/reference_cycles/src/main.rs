a0abd4caf4d00a77d1b61e563d33f1e1f56c8ff7
ccdle12 <chris.coverdale24@gmail.com>
2019-03-25 21:22:27 +0800

    15/smart_pointers: adding summary and use weak referencing in a tree

--- 15_smart_pointers/reference_cycles/src/main.rs
+++ 15_smart_pointers/reference_cycles/src/main.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use List::{Cons, Nil};
 
 fn main() {
@@ -27,10 +27,43 @@ fn main() {
     // it will overflow the stack
     // println!("a next item = {:?}", a.tail());
 
+    // Creating a Tree Data Structure.
+    // Leaf is a node that contains the value 3 and no children it is
+    // referencing.
+    //
+    // Here we update the Node to include a 'parent field' which stores a weak
+    // reference. This is so that we don't get the issues of reference cycles,
+    // by having a weak reference, leaf doesn't own branch but can reference it.
+    // If the branch is dropped from memory leaf will eb dropped as well.
+    // If leaf is dropped from memory, this does not affect it's parent.
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
+
+    // The reference to the parent is currently None.
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    // We create the node branch and clone leaf it it's children. This means
+    // that leaf has two owners, leaf and branch. We can access leaf via branch
+    // by 'branch.children'. Currently leaf has no knowledge of branch as it's
+    // parent.
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    // Downgrade creates a weak pointer to &branch.
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    // SUMMARY:
+    // Box<T> has a known size and points to data allocated on the heap.
+    // Rc<T> keeps track of the number of references to the data on the heap.
+    // RefCell<T> allows immutable and mutable references of data.
 }
 
 #[derive(Debug)]
@@ -51,5 +84,6 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
