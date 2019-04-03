9f626ffca7665c30c5e10ebd2329620392f34c60
ccdle12 <chris.coverdale24@gmail.com>
2019-04-03 23:42:53 +0800

    20: update

--- 20_webserver/hello/src/main.rs
+++ 20_webserver/hello/src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("failed to bind to port 7878");
@@ -8,6 +10,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // Create a buffer ont he stack capable of reading 512 bytes.
+    let mut buffer = [0; 512];
+
+    // Read the bytes from the stream and write it the buffer.
+    stream.read(&mut buffer).unwrap();
+
+    // Borrow the buffer and create a string from_utf8_lossy and print.
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
